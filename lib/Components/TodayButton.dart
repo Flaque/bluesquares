85e9dad25ddb69996d4cc36cc2c467499cbee08b
Flaque <econrad@zagmail.gonzaga.edu>
2018-06-03 20:07:12 -0600

    Refactor into FixedSideLayout

--- lib/Components/TodayButton.dart
+++ lib/Components/TodayButton.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
-
-const HEIGHT = 88.0;
-const TEXT_WIDTH = 88.0;
+import './FixedSideLayout.dart';
 
 class TodayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Row(children: <Widget>[
-      new Container(
-        width: TEXT_WIDTH,
-        alignment: Alignment.topLeft,
-        child: new Container(
-            alignment: Alignment.topLeft,
-            height: HEIGHT,
-            child: new Text('Today',
-                textAlign: TextAlign.start,
-                style: new TextStyle(
-                    color: Colors.redAccent,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 20.0))),
-      ),
-      new Expanded(
-          child: new Container(
-              height: HEIGHT,
-              child: new RaisedButton(
-                color: Colors.blue,
-                onPressed: () => {},
-              )))
-    ]);
+    return new FixedSideLayout(new Today(), new StreakButton());
+  }
+}
+
+class StreakButton extends StatelessWidget {
+  const StreakButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        height: FixedSideLayout.HEIGHT,
+        child: new RaisedButton(
+          color: Colors.blue,
+          onPressed: () => {},
+        ));
+  }
+}
+
+class Today extends StatelessWidget {
+  const Today({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text('Today',
+        textAlign: TextAlign.start,
+        style: new TextStyle(
+            color: Colors.redAccent,
+            fontWeight: FontWeight.w700,
+            fontSize: 20.0));
   }
 }
